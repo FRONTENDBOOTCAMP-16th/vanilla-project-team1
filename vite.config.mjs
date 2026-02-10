f08196f351031dc2d6f07ea31b4bf7a05e1b5054
ruiwaa <yeejii08@gmail.com>
2026-02-10 15:39:21 +0900

    chore/ 비트 환경 설정 (개발 서버 연결 파일 경로)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    open: '/src/page/payment/paymentPage.html',
   },
-})
+});
