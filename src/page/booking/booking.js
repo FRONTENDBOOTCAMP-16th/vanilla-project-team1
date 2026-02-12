6ac5da32a3392aff286c8237f3a1c3d36718ce26
JKCho <wornjs2362@gmail.com>
2026-02-12 11:45:13 +0900

    feature/renderMovieList 함수 작성

---
+++ src/page/booking/booking.js
@@ -0,0 +1,52 @@
+import { showtimeAPI } from '../../apis/apiRequest.js';
+
+const container = document.querySelector('.movie-list');
+
+function renderMovieList(movieList) {
+  container.innerHTML = '';
+
+  movieList.forEach((v) => {
+    console.log(v);
+    const article = document.createElement('article');
+    article.classList.add('movie-card');
+
+    const poster = document.createElement('img');
+    poster.src = '/src/assets/images/team_logo.png';
+    poster.alt = v.movieName;
+    poster.width = 100;
+    poster.height = 150;
+
+    const timeList = document.createElement('div');
+    timeList.classList.add('showtime-list');
+
+    v.timeTable.forEach((table) => {
+      const button = document.createElement('button');
+      button.classList.add('show-time');
+
+      button.innerHTML = `
+      <span class="hall">${table.screenName}</span>
+              <span class="time"><strong>${table.startTime}</strong>~${table.endTime}</span>
+              <span class="seats"><strong>${table.seatCount.available}</strong>/${table.seatCount.total}</span>
+              <span class="format">${table.format}</span>
+      `;
+
+      timeList.appendChild(button);
+    });
+
+    article.appendChild(poster);
+    article.appendChild(timeList);
+    container.appendChild(article);
+  });
+}
+
+async function loadShowTime() {
+  try {
+    const data = await showtimeAPI.list();
+    // console.log(data);
+    renderMovieList(data);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+loadShowTime();
