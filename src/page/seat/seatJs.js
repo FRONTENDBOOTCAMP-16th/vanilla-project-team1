7f129f8e7658fa97f634290b687f50a7da94e4ab
lcmm <lcmbook55@gmail.com>
2026-02-12 16:54:48 +0900

    feature/js 변수 SPECIAL 추가, 컬러 추가

--- src/page/seat/seatJs.js
+++ src/page/seat/seatJs.js
@@ -19,6 +19,8 @@
 // 평면 12열 11행
 const AVAILABLE = 1;
 const RESERVED = 2;
+const SPECIAL = 3;
+// const selected = 4;
 
 const seatArr = [
   [
@@ -26,8 +28,36 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+  ],
+  [
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    RESERVED,
+    RESERVED,
+  ],
+  [
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    RESERVED,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
@@ -55,10 +85,24 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+  ],
+  [
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    AVAILABLE,
+    RESERVED,
+    RESERVED,
+    AVAILABLE,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
@@ -74,41 +118,13 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    RESERVED,
+    RESERVED,
   ],
   [
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-  ],
-  [
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-  ],
-  [
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
@@ -125,21 +141,7 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-  ],
-  [
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
@@ -155,8 +157,8 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
@@ -167,13 +169,13 @@ const seatArr = [
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
+    RESERVED,
+    RESERVED,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    RESERVED,
   ],
   [
     AVAILABLE,
@@ -186,8 +188,8 @@ const seatArr = [
     RESERVED,
     AVAILABLE,
     AVAILABLE,
-    AVAILABLE,
-    AVAILABLE,
+    SPECIAL,
+    SPECIAL,
   ],
 ];
 
@@ -200,13 +202,27 @@ function renderSeat(seatArr) {
   const cols = seatArr[0].length;
   container.style.setProperty('--cols', String(cols));
 
+  // 12행i 11열j 이중for문 124석 배열 버튼연결해서 돌리기
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       const seatElement = document.createElement('button');
-      seatElement.textContent = seatArr[i][j];
+
+      // 행 (A, B...)
+      const rowLabel = String.fromCodePoint(65 + i);
+      // 열 (1, 2...)
+      const colLabel = j + 1;
+      seatElement.textContent = `${rowLabel}${colLabel}`;
+
       if (seatArr[i][j] === RESERVED) {
+        seatElement.classList.add('reserved'); // 클래스 추가해야함
         seatElement.style.color = 'lightgray';
+        seatElement.disabled = true;
+      } else if (seatArr[i][j] === SPECIAL) {
+        seatElement.classList.add('special');
+        seatElement.style.backgroundColor = 'green'; // 장애인석 컬러 다시 논의해서 수정해야함
       }
+
+      // 좌석 컨테이너에 추가했음
       container.appendChild(seatElement);
     }
   }
