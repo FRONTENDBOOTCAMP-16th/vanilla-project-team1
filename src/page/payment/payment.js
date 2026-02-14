c137df8cc3499b8e9d1a003700e78ce4833d5714
ruiwaa <yeejii08@gmail.com>
2026-02-14 20:50:51 +0900

    feature/js 폼 서식의 포인트 조건 미충족시 알림창 나오는 기능 추가

--- src/page/payment/payment.js
+++ src/page/payment/payment.js
@@ -5,9 +5,8 @@
 // [✅] 할인/포인트 버튼 클릭시 화면 전환
 // [✅] 폼 변환될 때 스타일링
 // [✅] 폼 서식에 비밀번호 입력 안했을 시 해당 알림창 나오게 하기
-// [] 폼 서식에 해당 값 적용하기
+// [] 폼 서식에 해당 값 적용하기 (해당 포인트 입력하고 적용 버튼 누르면 푸터 결제 영역에 보이게 하기)
 // [] 폼 서식 포인트 입력 및 비밀번호 입력 양식에 맞춰서 제어할 수 있게
-// [] 버튼 누르면 입력한 서식이 적용되게 하기
 // [✅] 할인 및 포인트 적립 버튼 접근성 속성 변환
 // [✅] 버튼 클릭 시 아리아 속성 전환
 // [] 체크박스 체크된 상태 일 때 추가 옵션 체크 박스 체크 할 수 있게
@@ -26,6 +25,8 @@ const FINAL_PAYMENT = document.querySelector('.final-payment');
 const FINAL_PAYMENT_METHODS_BUTTON = document.querySelectorAll('.final-payment-methods button');
 const CARD_POINT_PASSWORD_INPUT = document.getElementById('card-point-password');
 const CARD_NUMBER_PASSWORD_INPUT = document.getElementById('card-number-password');
+const POINT_INPUT_DATA = document.getElementById('point-input');
+const CURRENT_POINT_INPUT_DATA = document.getElementById('use-current-point');
 
 // 모든 요소의 active 클래스 네임 제거 함수
 function removeAllActive(elements, attrValue) {
@@ -42,6 +43,7 @@ function setAllAttr(elements, attrName, attrValue) {
 }
 // 활성화 클래스명 추가 함수
 function isActive(element) {
+  if (!element) return;
   element.classList.add('active');
 }
 
@@ -100,10 +102,42 @@ function handleFinalPaymentButton(e) {
   isActive(target);
 }
 
+// 폼 서식 포인트 입력 조건 미충족 시 알림창 나오게 하기
+// + 100 단위로 쓸 수 있게 하기
+// 패널 1
+function pointInputAuth(e) {
+  if (!e.target) return;
+  const target = e.target.closest('#point-input');
+  let value = parseInt(target.value, 10);
+  // 조건문 만들기
+  // 거짓: 숫자가 아니면 알림창 뜨기
+  // 참: 해당 값 표시
+  if (isNaN(value) || value % 100 !== 0) {
+    alert('100 단위로 포인트를 입력하세요');
+  }
+  if (value % 100 === 0) {
+    return value;
+  }
+}
+// 패널 2
+function currentInputAuth(e) {
+  if (!e.target) return;
+  const target = e.target.closest('#use-current-point');
+  let value = parseInt(target.value, 10);
+  // 조건문 만들기
+  // 거짓: 숫자가 아니면 알림창 뜨기
+  // 참: 해당 값 표시
+  if (isNaN(value) || value % 100 !== 0) {
+    alert('100 단위로 포인트를 입력하세요');
+  }
+  if (value % 100 === 0) {
+    return value;
+  }
+}
+
 // 폼 서식에 비밀번호 입력 안했을 시 해당 알림창 나오게 하기
 // 패널 1
 function lionPointCardPasswordAuth(e) {
-  e.preventDefault();
   if (!e.target) return;
   const target = e.target.closest('#card-point-password');
   if (target.value.length < 6 || target.value.length > 8) {
@@ -119,17 +153,20 @@ function lionPointCardNumberPasswordAuth(e) {
     alert('6~8자리로 입력하세요');
   }
 }
+
 // 폼 서식에 해당 값 적용하기
-// 폼 서식 포인트 입력 및 비밀번호 입력 양식에 맞춰서 제어할 수 있게
 // 버튼 누르면 입력한 서식이 적용되게 하기
 
 // 예매 티켓 결제 페이지 내부에 연결된 이벤트
-
 // 할인/포인트 버튼 클릭시 화면 전환 이벤트
 POINT_TAB.addEventListener('click', handleTabClick);
 
 // 힐인/포인트 방법 버튼 속성 및 스타일링 변환 이벤트
 COUPON_LIST.addEventListener('click', handleCouponList);
+
+// 폼 서식에 조건에 충족하는 포인트 입력하는 이벤트
+POINT_INPUT_DATA.addEventListener('change', pointInputAuth);
+CURRENT_POINT_INPUT_DATA.addEventListener('change', currentInputAuth);
 
 // 폼 서식에 비밀번호 입력시 알림창 나오는 이벤트
 // 패널 1
